3e4c72c97d7932af682f0908ec3039fc4a86159e
daredevil17052004 <anshs052004@gmail.com>
2024-07-18 02:44:11 +0530

    second page completed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,15 @@
-import { Inter } from "next/font/google";
+import { Inter, Montserrat } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@magiclabs/ui";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const mont = Montserrat({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['Arial', 'sans-serif'],
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +19,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} ${mont.className}`}>
+          {children}
+
+      </body> 
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,226 @@
+import NavigationBar from "./components/NavigationBar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Particles from "@/components/magicui/particles";
+import { Progress } from "@/components/ui/progress"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <NavigationBar />
+      <div className="bg-myBackground h-screen w-full mb-10">
+        <div className="mx-32 flex justify-between h-[500px] items-center">
+          {/* Review Section */}
+          <div className="w-1/2">
+            <div className="my-3 font-semibold">
+              <span className='text-myAccent'>|</span> <span>#1 Car Maintenance Service</span>
+            </div>
+            <div className="my-3">
+              <p className="text-4xl font-semibold">Expert Car Maintaining Service
+              </p>
+            </div>
+            <div className="my-3">
+              <p>Welcome to Maintainer, your one-stop-shop for premium car detailing services.</p>
+            </div>
+            <div className="flex my-3 justify-between w-3/4">
+              <div className="my-3">
+                <p>*****</p>
+                <p className="flex items-center"><span className="text-3xl font-semibold">4.8</span><span> / 5.0</span></p>
+              </div>
+              <div className="flex my-3 justify-between w-72 items-center">
+                <div>
+                  <Avatar className="h-16 w-16">
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                </div>
+                <div className="">
+                  <p className='text-3xl font-semibold '>24K</p>
+                  <p className="text-2xl ">Happy Customers</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Form */}
+          <div className='w-1/2'>
+            <form className="flex flex-col justify-between">
+              <div className="my-3">
+                <div className="mb-1">
+                  <label>Name</label>
+                </div>
+                <div>
+                  <Input placeholder='Ex. Joe Alan' />
+                </div>
+              </div>
+              <div className="my-3">
+                <div className="mb-1">
+                  <label>Email</label>
+                </div>
+                <div>
+                  <Input placeholder='Ex. abc@gmail.com' type='email' />
+                </div>
+              </div>
+              <div className="my-3">
+                <div className='mb-1'>
+                  <label>Phone Number</label>
+                </div>
+                <div>
+                  <Input placeholder='Ex. +91 1234567890' />
+                </div>
+              </div>
+              <div className="my-3">
+                <Button className="w-full bg-myAccent text-white" variant='ghost' type='submit'>Get Started</Button>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        {/* Companies */}
+        <div className="mx-32 mt-10">
+          <div>
+            Trusted by global companies
+          </div>
+          <div>
+            <div className="grid grid-cols-4 gap-4">
+              {[
+                '/Alpina_logo.svg',
+                '/Audi-Logo_2016.svg',
+                '/Maserati_logo_2.svg',
+                '/Prancing_horse.svg',
+                '/Ruf_Automobile_logo.svg',
+                '/BMW.svg',
+                '/Toyota.svg',
+                '/LandRover.svg'
+              ].map((src, index) => (
+                <div key={index} className="relative border-2 w-[300px] h-40 flex items-center justify-center">
+                  <Image src={src} width={100} height={100} />
+                  <Particles className="absolute inset-0" quantity={20} ease={80} color="#E33665" refresh />
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+
+      {/* next section */}
+
+
+      <div className="h-screen bg-black text-white flex flex-col items-center justify-center">
+        <div className="mx-32 ">
+          <div className="flex items-center justify-between ">
+            <div>
+              <Image src='/sec2.jpg' width={600} height={600} className="rounded" />
+            </div>
+            <div className=" w-1/2">
+              <div>
+                <p>About Us</p>
+              </div>
+              <div className='text-3xl'>
+                <p>Unmatched Maintaining Expertise</p>
+              </div>
+              <div className="my-3">
+                <label>Expert attention to detail</label>
+                <Progress value={80} className="w-full" />
+              </div>
+              <div className="my-3">
+                <label>State-of-the-art equipment</label>
+                <Progress value={93} className="w-full" />
+              </div>
+              <div className="my-3">
+                <label>Tailored services</label>
+                <Progress value={85} className="w-full" />
+              </div>
+              <div className="my-3">
+                <label>Exceptional customer service</label>
+                <Progress value={96} className="w-full" />
+              </div>
+            </div>
+          </div>
+
+
+          <div className="flex items-center  justify-center mt-10">
+            <div className="w-1/2">
+              <p className="font-semibold text-xl">Our Vision</p>
+              <p>
+                Our vision is to become the leading provider of premium car
+                detailing services in the region, delivering unparalleled results
+                and customer satisfaction.
+              </p>
+              <div className='mt-2'>
+                <Button className="bg-myAccent">Learn More</Button>
+              </div>
+            </div>
+            <div >
+              <div className="flex flex-col justify-between">
+                <div className="font-semibold text-xl mb-2">
+                  Our Mission
+                </div>
+                <div className="grid grid-cols-4 gap-4 mt-2">
+                  <div className="w-40">
+                    <div>
+                      <Image src='/rise (1).png' width={25} height={25} />
+                    </div>
+                    <div>
+                      <p>To provide
+                        unmatched car
+                        detailing services to
+                        our valued clients</p>
+                    </div>
+                  </div>
+                  <div className="w-40">
+                    <div>
+                      <Image src='/pie-chart.png' width={25} height={25} />
+                    </div>
+                    <div>
+                      <p>To exceed
+                        expectations with
+                        every vehicle we
+                        detail</p>
+                    </div>
+                  </div>
+                  <div className="w-40">
+                    <div>
+                      <Image src='/red-flag.png' width={25} height={25} />
+                    </div>
+                    <div>
+                      <p>To continually
+                        invest in the latest
+                        technology and
+                        techniques to
+                        deliver superior
+                        results</p>
+                    </div>
+                  </div>
+                  <div className="w-40">
+                    <div>
+                      <Image src='/monitor.png' width={25} height={25} />
+                    </div>
+                    <div>
+                      <p>To prioritize
+                        customer
+                        satisfaction and
+                        experience</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      {/* next Section */}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="h-screen w-full">
+        <div className="mx-32">
+        </div> 
       </div>
+
     </main>
   );
 }
